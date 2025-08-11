d068b631b3954b88a987bf6137d6f0c61be3a282
Vlad J <vladjdk@gmail.com>
2025-08-11 13:39:55 -0400

    Update evmd/tests/integration/create_app.go
    Co-authored-by: Eric Warehime <eric.warehime@gmail.com>

--- evmd/tests/integration/create_app.go
+++ evmd/tests/integration/create_app.go
@@ -21,7 +21,7 @@ import (
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
-// CreateEvmd creates an evmos app for regular integration tests (non-mempool)
+// CreateEvmd creates an evm app for regular integration tests (non-mempool)
 // This version uses a noop mempool to avoid state issues during transaction processing
 func CreateEvmd(chainID string, evmChainID uint64, customBaseAppOptions ...func(*baseapp.BaseApp)) evm.EvmApp {
 	defaultNodeHome, err := clienthelpers.GetNodeHomeDirectory(".evmd")
